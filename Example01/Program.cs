078679106b2e3417e7f6cdcb3be0342f207c6c5d
Arumarov <arumarov@yandex.ru>
2021-11-23 22:12:19 +0600

    Add Example01

---
+++ Example01/Program.cs
@@ -0,0 +1,52 @@
+﻿// // Двумерные массивы (вывод без метода)
+// int[,] a;
+// a=new int[6,8]; // создание массива 
+
+// for(int i=0; i<a.GetLength(0); i++)
+// {
+//    for(int j=0; j<a.GetLength(1); j++)
+//    {
+//        a[i,j]=i*10+j; // заполняем массив
+//    }
+// }
+
+// for(int i=0; i<a.GetLength(0); i++)
+// {
+//    for(int j=0; j<a.GetLength(1); j++)
+//    {
+//        Console.Write($"{a[i,j],4}"); // Печатаем, форматируем - выделяем 4 символа для каждого аргумента
+//    }
+//    Console.WriteLine();
+// }
+
+
+// Двумерные массивы (вывод с помощью метода)
+
+void SetArray2D(int[,] a) // метод заполнения массива
+{
+for(int i=0; i<a.GetLength(0); i++)
+{
+   for(int j=0; j<a.GetLength(1); j++)
+   {
+       a[i,j]=i*10+j; // заполняем массив
+   }
+}
+}
+
+void Print(int[,] a) // метод печати массива
+{
+for(int i=0; i<a.GetLength(0); i++)
+{
+   for(int j=0; j<a.GetLength(1); j++)
+   {
+       Console.Write($"{a[i,j],4}"); // Печатаем, форматируем - выделяем 4 символа для каждого аргумента
+   }
+   Console.WriteLine();
+}
+}
+
+int[,] a;
+a=new int[6,8]; // создание массива
+
+SetArray2D(a);
+Print(a);
