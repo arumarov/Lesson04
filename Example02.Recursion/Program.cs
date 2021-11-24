6d61c8f553dd5f62c4ae385de71a8f6b23666b76
Arumarov <arumarov@yandex.ru>
2021-11-24 22:16:17 +0600

    Add Example02

---
+++ Example02.Recursion/Program.cs
@@ -0,0 +1,32 @@
+﻿// Вывести на экран числа от 1 до 10, не используя операторы цикла
+
+// void Loop(int i)
+// {
+//     Console.Write($"{i} ");
+//     if (i<10) Loop(i+1);
+// }
+
+// Loop(1);
+
+
+// // Вывод факториала с применением рекурсии
+
+// ulong F(ulong n)
+// {
+//     if (n==0) return 1;
+//     else return F(n-1)*n;
+// }
+
+// Console.WriteLine(F(10));
+
+// // Вывод чисел Фибоначчи с применением рекурсии
+// Мое решение (вызов последнего числа Фибоначчи)
+ulong Fibo(ulong n)
+{
+    if (n==0) return 1;
+    if (n==1) return 1;
+    else return Fibo(n-1)+Fibo(n-2);
+
+}
+
+Console.WriteLine(Fibo(5));
