2dbf425e03a51149b6635ee6cb1fddc2edb9af26
Arumarov <arumarov@yandex.ru>
2021-11-25 22:30:31 +0600

    Add Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,5 +1,6 @@
 ﻿// ЗАДАЧА 2. Дан целочисленный двумерный массив, размерности n х m. Найти сумму и произведение четных элементов
-void SetArray2D(int[,] a) 
+// Мое решение
+/*void SetArray2D(int[,] a) 
 {
 for(int i=0; i<a.GetLength(0); i++)
 {
@@ -54,4 +55,52 @@ a=new int[5,5];
 SetArray2D(a);
 Print(a);
 Console.Write("Сумма четных элементов: " + $"{SumEven(a)}");
-Console.Write("\nПроизведение четных элементов: " + $"{MultiEven(a)}");
+Console.Write("\nПроизведение четных элементов: " + $"{MultiEven(a)}");*/
+
+//Решение с семинара
+
+void SetArray2D(int[,] a) 
+{
+for(int i=0; i<a.GetLength(0); i++)
+{
+   for(int j=0; j<a.GetLength(1); j++)
+   {
+       a[i,j]=new Random().Next(1,99); 
+   }
+}
+}
+
+void Print(int[,] a) 
+{
+for(int i=0; i<a.GetLength(0); i++)
+{
+   for(int j=0; j<a.GetLength(1); j++)
+   {
+       Console.Write($"{a[i,j],4}"); 
+   }
+   Console.WriteLine();
+}
+}
+
+void Task02(int[,] a, out int sum, out int multi) 
+{
+    sum = 0;
+    multi = 1;
+   for(int i=0; i<a.GetLength(0); i++)
+{
+   for(int j=0; j<a.GetLength(1); j++)
+   {
+      if (a[i,j]%2==0) {sum = sum+a[i,j]; multi = multi*a[i,j];};
+   }
+}
+}
+
+int[,] a;
+a=new int[5,5]; 
+
+SetArray2D(a);
+Print(a);
+int s,m;
+Task02(a,out s,out m);
+Console.Write("Сумма четных элементов: " + $"{s}");
+Console.Write("\nПроизведение четных элементов: " + $"{m}");
