75cf3107d785eef3935cb776784ca5681158cac6
Arumarov <arumarov@yandex.ru>
2021-11-25 21:42:40 +0600

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,59 @@
+﻿// ЗАДАЧА 3. Дан целочисленный двумерный массив, размерности n х m. Найти сумму и произведение элементов, кратных 3 и 5.
+
+// Мое решение
+void SetArray2D(int[,] a) 
+{
+for(int i=0; i<a.GetLength(0); i++)
+{
+   for(int j=0; j<a.GetLength(1); j++)
+   {
+       a[i,j]=new Random().Next(1,99); 
+   }
+}
+}
+
+void Print(int[,] a) 
+{
+for(int i=0; i<a.GetLength(0); i++)
+{
+   for(int j=0; j<a.GetLength(1); j++)
+   {
+       Console.Write($"{a[i,j],4}"); 
+   }
+   Console.WriteLine();
+}
+}
+
+int Sum3_5(int[,] a) 
+{
+int result = 0;
+for(int i=0; i<a.GetLength(0); i++)
+{
+   for(int j=0; j<a.GetLength(1); j++)
+   {
+        if (a[i,j]%3==0 && a[i,j]%5==0) result = result + a[i,j];
+   }
+}
+   return result;
+}
+
+double Multi3_5(int[,] a) 
+{
+double result = 1;
+for(int i=0; i<a.GetLength(0); i++)
+{
+   for(int j=0; j<a.GetLength(1); j++)
+   {
+       if (a[i,j]%3==0 && a[i,j]%5==0) result = result * a[i,j]; 
+   }
+}
+   return result;
+}
+
+int[,] a;
+a=new int[10,10]; 
+
+SetArray2D(a);
+Print(a);
+Console.Write("Сумма элементов, кратных 3 и 5: " + $"{Sum3_5(a)}");
+Console.Write("\nПроизведение элементов, кратных 3 и 5: " + $"{Multi3_5(a)}");
