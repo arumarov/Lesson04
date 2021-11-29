d0a618af6118ae67715ac2bec5837579add474a6
Arumarov <arumarov@yandex.ru>
2021-11-29 22:07:08 +0600

    Add Task08

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -45,44 +45,44 @@ Quantity(a);
 
 
 //Решение с семинара
-void SetArray2D(int[,] a) 
-{
-for(int i=0; i<a.GetLength(0); i++)
-{
-   for(int j=0; j<a.GetLength(1); j++)
-   {
-       a[i,j]=new Random().Next(1,99); 
-   }
-}
-}
+// void SetArray2D(int[,] a) 
+// {
+// for(int i=0; i<a.GetLength(0); i++)
+// {
+//    for(int j=0; j<a.GetLength(1); j++)
+//    {
+//        a[i,j]=new Random().Next(1,99); 
+//    }
+// }
+// }
 
-void Print(int[,] a) 
-{
-for(int i=0; i<a.GetLength(0); i++)
-{
-   for(int j=0; j<a.GetLength(1); j++)
-   {
-       Console.Write($"{a[i,j],4}"); 
-   }
-   Console.WriteLine();
-}
-}
+// void Print(int[,] a) 
+// {
+// for(int i=0; i<a.GetLength(0); i++)
+// {
+//    for(int j=0; j<a.GetLength(1); j++)
+//    {
+//        Console.Write($"{a[i,j],4}"); 
+//    }
+//    Console.WriteLine();
+// }
+// }
 
-void Task05(int[,] a) 
-{
+// void Task05(int[,] a) 
+// {
 
-   for(int i=0; i<a.GetLength(0); i++)
-   {
-      for(int j=0; j<a.GetLength(1); j++)
-        {
-            if (i%2==0 && j%2==0 && a[i,j]%2!=0) {Console.WriteLine($"{i,2},{j,2} = {a[i,j],4}");};
-        }
-   }
-}
+//    for(int i=0; i<a.GetLength(0); i++)
+//    {
+//       for(int j=0; j<a.GetLength(1); j++)
+//         {
+//             if (i%2==0 && j%2==0 && a[i,j]%2!=0) {Console.WriteLine($"{i,2},{j,2} = {a[i,j],4}");};
+//         }
+//    }
+// }
 
-int[,] a;
-a=new int[5,5]; 
+// int[,] a;
+// a=new int[5,5]; 
 
-SetArray2D(a);
-Print(a);
-Task05(a);
+// SetArray2D(a);
+// Print(a);
+// Task05(a);
---
+++ Task08/Program.cs
@@ -0,0 +1,45 @@
+﻿// ЗАДАЧА 8. Дан целочисленный двумерный массив, размерности n х m. Найти среднее арифметическое всех элементов массива
+
+void SetArray2D (int [,] a)
+{
+    for (int i=0;i<a.GetLength(0);i++)
+    {
+        for (int j=0;j<a.GetLength(1);j++)
+        {
+            a[i,j]=new Random().Next(1,99);
+        }
+    }
+}
+
+void PrintArray (int[,] a)
+{
+    for (int i=0;i<a.GetLength(0);i++)
+    {
+        for (int j=0;j<a.GetLength(1);j++)
+        {
+            Console.Write($"{a[i,j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Average (int[,] a)
+{
+    int temp=0;
+    for (int i=0;i<a.GetLength(0);i++)
+    {
+        for (int j=0;j<a.GetLength(1);j++)
+        {
+            temp=temp+a[i,j];
+        }
+
+    }
+    return temp/100;
+}
+
+int[,] a;
+a = new int[10,10];
+SetArray2D(a);
+PrintArray(a);
+Console.WriteLine("Среднее арифметическое всех элементов массива: ");
+Console.Write(Average(a));
