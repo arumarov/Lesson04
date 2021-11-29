1a5dfc4c96fd5045816f756e732baf569c244f56
Arumarov <arumarov@yandex.ru>
2021-11-29 22:38:03 +0600

    Add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,48 @@
+﻿// ЗАДАЧА 11. Дан целочисленный двумерный массив, размерности n х m. 
+// Вывести его элементы, индексы которых являются степенями двойки (1, 2, 4, 8, 16, ...)
+
+void SetArray2D (int [,] a)
+{
+    for (int i=0;i<a.GetLength(0);i++)
+    {
+        for (int j=0;j<a.GetLength(1);j++)
+        {
+            a[i,j]=new Random().Next(1,99);
+        }
+    }
+}
+
+void PrintArray (int[,] a)
+{
+    for (int i=0;i<a.GetLength(0);i++)
+    {
+        for (int j=0;j<a.GetLength(1);j++)
+        {
+            Console.Write($"{a[i,j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArray2 (int[,] a)
+{
+    for (int i=0;i<a.GetLength(0);i++)
+    {
+        for (int j=0;j<a.GetLength(1);j++)
+        {
+            for (int x=0;x<10;x++)
+            {
+                if (i==(Math.Pow(2,x-1)) || j==(Math.Pow(2,x-1))) Console.Write($"{a[i,j],4}");
+            }
+
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] a;
+a = new int[10,10];
+SetArray2D(a);
+PrintArray(a);
+Console.WriteLine("Элементы, индексы которых являются степенями двойки: ");
+PrintArray2(a);
